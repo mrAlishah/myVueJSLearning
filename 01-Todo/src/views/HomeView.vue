cc524d5215a03d7fee15fc69e19d94b0bcd60e7d
mrAlishah <mrAlishPrg@gmail.com>
2025-01-31 10:40:46 +0100

    Wrapper::slot

--- 01-Todo/src/views/HomeView.vue
+++ 01-Todo/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TodoItem from '@/components/TodoItem.vue'
+import Wrapper from '@/components/Wrapper.vue'
 
 const todos = [
   {
@@ -23,7 +24,34 @@ const todos = [
     <button>Show Todo</button>
 
     <div v-for="todo in todos" :key="todo.id">
-      <TodoItem :todo="todo" />
+      <Wrapper>
+        <TodoItem :todo="todo" />
+      </Wrapper>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  button {
+    color: #fff;
+    background: #1e40af;
+    padding: 4px 15px;
+    border-radius: 5px;
+    &:hover {
+      background: #0ea5e9;
+    }
+  }
+}
+</style>
