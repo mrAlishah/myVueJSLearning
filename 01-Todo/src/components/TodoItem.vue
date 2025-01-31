9a52c501d242c7f7f1a93ab6096e42853b5ef153
mrAlishah <mrAlishPrg@gmail.com>
2025-01-31 10:01:10 +0100

    Adding component and for

---
+++ 01-Todo/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>
+          {{ todo.title}}
+        </div>
+    </div>  
+</template>
+
+<script setup>
+ 
+ defineProps({
+    todo:{
+      type:Object,
+      required:true  
+    }
+ })
+</script>
+
+<style lang="scss" scoped>
+</style>
--- 01-Todo/src/views/HomeView.vue
+++ 01-Todo/src/views/HomeView.vue
@@ -1,7 +1,31 @@
 <script setup>
+import TodoItem from "@/components/TodoItem.vue"
+
+const todos=[
+    {
+        id:1,
+        title: "a",
+        done:true
+    },
+    {
+        id:2,
+        title: "b",
+        done:true
+    }
+]
 
 </script>
 
 <template>
+    <div class="header">
+       <div >
+        <h3>Todo</h3>
+       </div> 
+       <button>Show Todo</button>
+
+       <div v-for="todo in todos" :key="todo.id">
+        <TodoItem :todo="todo" />
+       </div>
+    </div> 
 
 </template>
