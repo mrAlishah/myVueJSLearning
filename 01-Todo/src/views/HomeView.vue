8a7c20f07f3796c9b688cd453e5f5b0fefa808e8
mrAlishah <mrAlishPrg@gmail.com>
2025-03-14 16:17:29 +0100

    Todo:: use ref to change buttone and title caption

--- 01-Todo/src/views/HomeView.vue
+++ 01-Todo/src/views/HomeView.vue
@@ -2,20 +2,28 @@
 import TodoItem from '@/components/TodoItem.vue'
 import Wrapper from '@/components/Wrapper.vue'
 import { useTodoStore } from '@/stores/TodoStore'
+import { ref } from 'vue'
 
 const todoStore = useTodoStore()
+
+const todoFilter = ref('all')
+const setTodoFilter = () => {
+  todoFilter.value = todoFilter.value === 'all' ? 'done' : 'all'
+}
 </script>
 
 <template>
   <div>
     <div class="header">
       <div>
-        <h3>Todo</h3>
+        <h3>{{ todoFilter === 'all' ? 'All todo' : 'Done todo' }}</h3>
       </div>
-      <button>Show Todo</button>
+      <button @click="setTodoFilter">
+        {{ todoFilter === 'all' ? 'Show done todo' : 'Show all todo' }}
+      </button>
     </div>
 
-    <div v-for="todo in todoStore.done" :key="todo.id">
+    <div v-for="todo in todoStore.sorted" :key="todo.id">
       <Wrapper>
         <TodoItem :todo="todo" />
       </Wrapper>
